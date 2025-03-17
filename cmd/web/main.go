8ba8161e84a4dc8e778edc3e5dc1d3d08be11a18
Juho Choi <juho.c98@gmail.com>
2025-03-17 11:56:37 -0400

    3.1. Managing configuration settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,42 +1,29 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Define a new command-line flag with the name 'addr', a default value of ':4000' and some short help text explaining what the flag controls. The value of the flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	
+	// Importantly, we use the flag.Parse() function to parse the command-line flag. This reads in the command-line flag value and assigns it to the addr variable. You need to call this before you use the addr variable otherwise it will always contain the default value of ':4000'. If any errors are encountered during parsing the application will be terminated
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
-	// Create a file server which serves files out of the
-	// "./ui/static" directory. Note that the path given to the
-	// http.Dir function is relative to the project directory root
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as
-	// the handler for all URL paths that start with "/static". For
-	// mathcing paths, we strip the "/static" prefix before the
-	// request reaches the file server.
+	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	// Register the other application routes as normal
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	// The value returned from the flag.String() function is a pointer to the flag value, not the value itself. So we need to dereference the pointer before using it.
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
-
-
-/*
-브라우저 URL 요청 → 서버 요청 처리 과정:
-(1) http://localhost:4000/static/css/main.css
-        ↓
-(2) mux.Handle("/static/", ...) 를 통해 "/static" 경로 확인.
-        ↓
-(3) http.StripPrefix("/static", ...) 가 "/static" 제거 후, "/css/main.css"로 전달.
-        ↓
-(4) fileServer 핸들러가 "./ui/static/css/main.css" 경로에서 실제 파일을 찾아 응답.
-*/
