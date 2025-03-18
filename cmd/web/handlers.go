10bd3daa49bb417dceb1011ac6f37413e2ff2278
Juho Choi <juho.c98@gmail.com>
2025-03-18 14:34:18 -0400

    3.3. Dependency injection

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,57 +3,41 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the home handler so it is defined as a method against *application
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
-	// Initialize a slice containing the paths to the two files. It's
-	// important to note that the file containing our base template
-	// must be the first file in the slice
 	files := []string{
 		"./ui/html/base.tmpl",
 		"./ui/html/partials/nav.tmpl",
 		"./ui/html/pages/home.tmpl",
 	}
 
-	// Use the template.ParseFiles() function to read the template
-	// file into a template set. If there is an error, we log the
-	// detailed error message and use the http.Error() function to
-	// send a generic 500 Internal Server Error response to the user
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
+		// Because the home handler function is now a method against application, it can access its fields including the error logger. We'll write the log message to this instead of the standard logger
+		app.errorLog.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
-	// Use the ExecuteTemplate() method to write the content of the
-	// base template as the response body
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
+		// Also update the code here to use the error logger from the application struct
+		app.errorLog.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
-
-	// We then use the Execute() method on the template set to write
-	// the template content as the response body. The last parameter
-	// to Execute() represents any dynamic data that we want to pass
-	// in, which for now we'll leave as nil
-	// err = ts.Execute(w, nil)
-	// if err != nil {
-	// 	log.Print(err.Error())
-	// 	http.Error(w, "Internal Server Error", 500)
-	// }
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the snippetView handler so it is defined as a method against *application
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -63,7 +47,8 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the snippetCreate handler so it is defined as a method against *application
+func (app *application)snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
